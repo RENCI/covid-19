d3d9e10e794a56d4bce5e6d30aa913b4142ed160
Matt Watson <mattwat@gmail.com>
2020-04-04 22:30:29 -0400

    add social account link placeholders to layout footer

--- src/components/layout/default/index.js
+++ src/components/layout/default/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 import PropTypes from 'prop-types'
 import { Header } from './header'
 import { Brand } from '../../brand'
@@ -11,9 +11,18 @@ import { useLayoutContext } from '../../../contexts'
 import { useMenu } from '../../../hooks'
 import { Container as Grid, Row, Col } from 'react-grid-system'
 import { Paragraph } from '../../typography'
-
+import { ExternalLink } from '../../link'
+import { FacebookIcon, LinkedInIcon, TwitterIcon } from '../../icons'
 import '../../../styles/normalize.css'
 import '../../../styles/customize.css'
+
+const SocialLink = styled(ExternalLink)`
+    margin: 0 1rem;
+    transition: filter 250ms;
+    &:hover {
+        filter: brightness(1.5);
+    }
+`
 
 const LayoutWrapper = styled.div(({ theme }) => `
     background-color: #f9fafc;
@@ -25,6 +34,7 @@ const LayoutWrapper = styled.div(({ theme }) => `
 `)
 
 export const DefaultLayout = ({ children }) => {
+    const theme = useTheme()
     const menuItems = useMenu()
     const { isCompact } = useLayoutContext()
     return (
@@ -55,6 +65,21 @@ export const DefaultLayout = ({ children }) => {
                             </Col>
                         </Row>
                     </Grid>
+                    <Row>
+                        <Col xs={ 12 }>
+                            <Paragraph center>
+                                <SocialLink to="https://tiwtter.com/renci">
+                                    <TwitterIcon size={ 32 } fill={ theme.color.primary.light } />
+                                </SocialLink>
+                                <SocialLink to="https://facebook.com/renci">
+                                    <FacebookIcon size={ 32 } fill={ theme.color.primary.light } />
+                                </SocialLink>
+                                <SocialLink to="https://linkedin.com/renci">
+                                    <LinkedInIcon size={ 32 } fill={ theme.color.primary.light } />
+                                </SocialLink>
+                            </Paragraph>
+                        </Col>
+                    </Row>
                 </Container>
             </Footer>
         </LayoutWrapper>
