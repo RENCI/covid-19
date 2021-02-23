54220306118cf22edfb8a14ef228a885de156fc6
Matt Watson <mattwat@gmail.com>
2021-02-23 15:11:25 -0500

    add reccap
    add content to our work view
    
    add reccap
    
    remove accidental edit

---
+++ src/data/work/reccap/index.md
@@ -0,0 +1,12 @@
+---
+path: /reccap
+title: ReCCAP
+blurb: >
+    RENCI researchers are supporting the ReCCAP UNC study to assess the effectiveness of COVID-19 preventive measures in keeping UNC workers safe as labs re-open across campus. The study is helping to advance research by offering free testing for COVID-19 antibodies and virus to members of UNC’s extended research community to monitor disease outbreak and reduce virus transmission.
+---
+
+RENCI researchers are supporting the [ReCCAP UNC](https://tracs.unc.edu/index.php/news-articles/1704-covid-19-study-recruiting-unc-faculty-students-and-staff-involved-with-research-on-campus) study to assess the effectiveness of COVID-19 preventive measures in keeping UNC workers safe as labs re-open across campus. The study is helping to advance research by offering free testing for COVID-19 antibodies and virus to members of UNC’s extended research community to monitor disease outbreak and reduce virus transmission.
+
+As students and workers return across the nation, it’s vital that we understand more about transmission on campus. This knowledge will inform best practices for testing methods and safety measures, and will help assess the usefulness of asymptomatic testing and contact tracing. The effort combines RENCI’s expertise in data science with UNC experts from the Depts. of Epidemiology, Infectious Diseases, Biology, and Microbiology and Immunology.
+
+Providing insights that make our communities safer is a key part of RENCI’s mission, and we’re even providing work space for the study as a sample collection and training site while our employees work from home. 
--- src/pages/work.js
+++ src/pages/work.js
@@ -10,39 +10,39 @@ import { useWork } from '../hooks'
 import { kebabCase } from '../util'
 
 const WorkPage = () => {
-    const work = useWork()
+  const work = useWork()
 
-    return (
-        <PageContent>
-            <SEO 
-                title="Our Work"
-                description=""
-                keywords=""
-            />
-            
-            <TitleCard title="Our Work">
-                <Paragraph>
-                    We will update this page with the latest projects coming out of RENCI as our researchers and their partners work together to develop novel approaches for dealing with COVID-19.
-                </Paragraph>
-            </TitleCard>
-            
-            {
-                work.map(item => (
-                    <Card key={ kebabCase(item.frontmatter.title) }>
-                        <CardHeader>{ item.frontmatter.title }</CardHeader>
-                        <CardBody>
-                            <div dangerouslySetInnerHTML={{ __html: item.frontmatter.blurb }} />
-                            <br/>
-                            <Paragraph center>
-                                <LinkButton to={ item.frontmatter.path }>Read More</LinkButton>
-                            </Paragraph>
-                        </CardBody>
-                    </Card>
-                ))
-            }
+  return (
+    <PageContent>
+      <SEO 
+        title="Our Work"
+        description=""
+        keywords=""
+      />
+      
+      <TitleCard title="Our Work">
+        <Paragraph>
+          We will update this page with the latest projects coming out of RENCI as our researchers and their partners work together to develop novel approaches for dealing with COVID-19.
+        </Paragraph>
+      </TitleCard>
+      
+      {
+        work.map(item => (
+          <Card key={ kebabCase(item.frontmatter.title) }>
+            <CardHeader>{ item.frontmatter.title }</CardHeader>
+            <CardBody>
+              <div dangerouslySetInnerHTML={{ __html: item.frontmatter.blurb }} />
+              <br/>
+              <Paragraph center>
+                <LinkButton to={ item.frontmatter.path }>Read More</LinkButton>
+              </Paragraph>
+            </CardBody>
+          </Card>
+        ))
+      }
 
-        </PageContent>
-    )
+    </PageContent>
+  )
 }
 
 export default WorkPage
