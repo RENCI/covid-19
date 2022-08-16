975a885888ce36b148fbb4ce838c26200ef2370d
Suejin Kim <kim.suejin.20@gmail.com>
2022-08-16 11:19:21 -0400

    remove old logo query

--- src/hooks/use-brand.js
+++ src/hooks/use-brand.js
@@ -11,37 +11,7 @@ const imageQuery = graphql`
     }
 `
 
-const logoQuery = graphql`{
-    allImageSharp(filter: {fluid: {originalImg: {regex: "/renci.png/"}}}) {
-        edges {
-            node {
-                id
-                fluid(maxWidth: 180) {
-                    base64
-                    tracedSVG
-                    srcWebp
-                    srcSetWebp
-                    originalImg
-                    originalName
-                    presentationWidth
-                    presentationHeight
-                }
-                fixed {
-                    base64
-                    tracedSVG
-                    aspectRatio
-                    srcWebp
-                    srcSetWebp
-                    originalName
-                }
-            }
-        }
-    }
-}`
-
 export const useBrand = () => {
     const data = useStaticQuery(imageQuery)
-    // const fluid = data.allImageSharp.edges[0].node.fluid
-    // const fixed = data.allImageSharp.edges[0].node.fixed
     return { data }
 }
